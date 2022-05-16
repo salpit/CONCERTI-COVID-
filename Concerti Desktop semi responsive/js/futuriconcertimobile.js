e28f4d6f0a6bb76b3d16d8cbca032b866fb5fef5
Salvo Pitingaro <86733544+salpit@users.noreply.github.com>
2022-05-16 21:35:10 +0200

    Add files via upload

---
+++ Concerti Desktop semi responsive/js/futuriconcertimobile.js
@@ -0,0 +1,63 @@
+var sheet = document.createElement('style'),  
+  $rangeInput = $('.range input'),
+  prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
+
+document.body.appendChild(sheet);
+
+var getTrackStyle = function (el) {  
+  var curVal = el.value,
+      val = (curVal - 1) * 16.666666667,
+      style = '';
+
+      if(curVal==1){
+        document.getElementById('polygon1').setAttribute('points', '314.25 450 0 450 0 0 314.25 450');
+        document.getElementById("numeroconcerti").innerHTML = "182";
+     }else if (curVal==2){
+      document.getElementById('polygon1').setAttribute('points', '314.25 450 0 450 40 0 314.25 450');
+      document.getElementById("numeroconcerti").innerHTML = "101";
+    }else if (curVal==3){
+      document.getElementById('polygon1').setAttribute('points', '314.25 450 0 450 85 0 314.25 450');
+      document.getElementById("numeroconcerti").innerHTML = "222";
+    }else if (curVal==4){
+      document.getElementById('polygon1').setAttribute('points', '314.25 450 0 450 125 0 314.25 450');
+      document.getElementById("numeroconcerti").innerHTML = "85";
+    }else if (curVal==5){
+      document.getElementById('polygon1').setAttribute('points', '314.25 450 0 450 167 0 314.25 450');
+      document.getElementById("numeroconcerti").innerHTML = "62";
+    }else if (curVal==6){
+      document.getElementById('polygon1').setAttribute('points', '314.25 450 0 450 210 0 314.25 450');
+      document.getElementById("numeroconcerti").innerHTML = "71";
+    }else if (curVal==7){
+      document.getElementById('polygon1').setAttribute('points', '314.25 450 0 450 250 0 314.25 450');
+      document.getElementById("numeroconcerti").innerHTML = "62";
+    }else if (curVal==8){
+      document.getElementById('polygon1').setAttribute('points', '314.25 450 0 450 290 0 314.25 450');
+      document.getElementById("numeroconcerti").innerHTML = "35";
+    }
+  
+
+  
+  // Set active label
+  $('.range-labels li').removeClass('active selected');
+  
+  var curLabel = $('.range-labels').find('li:nth-child(' + curVal + ')');
+  
+  curLabel.addClass('active selected');
+  curLabel.prevAll().addClass('selected');
+  
+
+  return style;
+}
+
+$rangeInput.on('input', function () {
+  sheet.textContent = getTrackStyle(this);
+});
+
+// Change input value on label click
+$('.range-labels li').on('click', function () {
+  var index = $(this).index();
+  
+  $rangeInput.val(index + 1).trigger('input');
+  
+});
+
