77ad385b2ff8be480c171eb71f213d18ddb083ba
Salvo Pitingaro <86733544+salpit@users.noreply.github.com>
2022-05-16 21:28:51 +0200

    Add files via upload

---
+++ Concerti_last_uptade/js/scriptbiglietti.js
@@ -0,0 +1,103 @@
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 4) {
+        $('.biglietto1').addClass('animate1 ');
+    } else {
+        $('.biglietto1').removeClass('animate1');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 8) {
+        $('.biglietto2').addClass('animate2 ');
+    } else {
+        $('.biglietto2').removeClass('animate2');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 12) {
+        $('.biglietto3').addClass('animate3');
+    } else {
+        $('.biglietto3').removeClass('animate3');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 16) {
+        $('.biglietto4').addClass('animate4');
+    } else {
+        $('.biglietto4').removeClass('animate4');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 20) {
+        $('.biglietto5').addClass('animate5');
+    } else {
+        $('.biglietto5').removeClass('animate5');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 24) {
+        $('.biglietto6').addClass('animate6');
+    } else {
+        $('.biglietto6').removeClass('animate6');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 28) {
+        $('.biglietto7').addClass('animate7');
+    } else {
+        $('.biglietto7').removeClass('animate7');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 32) {
+        $('.biglietto8').addClass('animate8');
+    } else {
+        $('.biglietto8').removeClass('animate8');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 34) {
+        $('.biglietto9').addClass('animate9');
+    } else {
+        $('.biglietto9').removeClass('animate9');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 38) {
+        $('.biglietto10').addClass('animate10');
+    } else {
+        $('.biglietto10').removeClass('animate10');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 42) {
+        $('.biglietto11').addClass('animate11');
+    } else {
+        $('.biglietto11').removeClass('animate11');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 46) {
+        $('.biglietto12').addClass('animate12');
+    } else {
+        $('.biglietto12').removeClass('animate12');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.biglietto13').addClass('animate13');
+    } else {
+        $('.biglietto13').removeClass('animate13');
+    }
+});
